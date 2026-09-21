a44aa5b3796c161b9910616f0c8d6bf681e6b6c2
“Tamara <tamara.kozok@bakers.studio>
2026-09-21 13:06:22 +0300

    chore: initialize Prisma and database configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller.js';
 import { AppService } from './app.service.js';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
