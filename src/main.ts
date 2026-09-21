8cc530dbef2efe4d6bc6aeb58b359544f5596ded
“Tamara <tamara.kozok@bakers.studio>
2026-09-21 19:48:27 +0300

    fix: bind app to all network interfaces for better accessibility

--- src/main.ts
+++ src/main.ts
@@ -15,7 +15,7 @@ async function bootstrap() {
 
   SwaggerModule.setup('api/docs', app, documentFactory);
 
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT ?? 3000, '0.0.0.0');
 }
 
 await bootstrap();
