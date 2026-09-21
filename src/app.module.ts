dfe5c32351627543aa77fffbe271dcb2f9c5fb18
“Tamara <tamara.kozok@bakers.studio>
2026-09-21 13:44:58 +0300

    feat: update and integrate Prisma module and service

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,14 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller.js';
 import { AppService } from './app.service.js';
 import { ConfigModule } from '@nestjs/config';
+import { PrismaModule } from './lib/database/prisma.module.js';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
     }),
+    PrismaModule,
   ],
   controllers: [AppController],
   providers: [AppService],
